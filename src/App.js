cc695b0bf6d6e7913b62dd4bce1eed39820eba2f
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-02-02 13:08:06 +0100

    Main page test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
+import Main from "./components/mainPage/Main";
 
 const App = () => {
 	return (
@@ -13,6 +14,7 @@ const App = () => {
 					<Route exact path="/">
 						<Redirect to="/main" />
 					</Route>
+					<Route path="/main" render={(props) => <Main />} />
 					<Route path="/jobs" />
 					<Route path="/fav" />
 				</div>
---
+++ src/components/mainPage/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Main = () => {
+	return (
+		<div>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus semper laoreet. Nulla ornare ipsum sem. Cras
+			dapibus, nisl eget lobortis aliquet, sapien lorem suscipit justo, vel facilisis massa tellus quis metus. Proin
+			sagittis magna vel rhoncus pellentesque. Nam mollis rhoncus vestibulum. Lorem ipsum dolor sit amet, consectetur
+			adipiscing elit. Proin non venenatis massa. Sed feugiat orci imperdiet ex pulvinar viverra.
+		</div>
+	);
+};
+
+export default Main;
