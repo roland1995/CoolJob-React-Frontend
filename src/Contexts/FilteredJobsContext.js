9fe3c9f9b7149981862822ab1200f938139df511
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-02-04 15:14:24 +0100

    Created filtered job context for the filtering functions

---
+++ src/Contexts/FilteredJobsContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from 'react';
+
+export const FilteredJobContext = createContext();
+
+export const FilteredJobProvider = (props) => {
+	const [filteredJobs, setFilteredJobs] = useState([]);
+
+	return (
+		<FilteredJobContext.Provider value={[filteredJobs, setFilteredJobs]}>
+			{props.children}
+		</FilteredJobContext.Provider>
+	);
+};
+
+export default FilteredJobProvider;
